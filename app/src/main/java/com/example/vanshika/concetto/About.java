de8fc32512fa6ae2a56c0ed26b3861c33aec6a3e
vanshikaarora <vanshikaa937@gmail.com>
2018-10-05 00:43:00 +0530

    added animations

--- app/src/main/java/com/example/vanshika/concetto/About.java
+++ app/src/main/java/com/example/vanshika/concetto/About.java
@@ -2,12 +2,16 @@ package com.example.vanshika.concetto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class About extends AppCompatActivity {
-
+Animation openScale;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        openScale= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.activity_open_scale);
+        overridePendingTransition(R.anim.activity_open_scale,R.anim.activity_close_scale);
     }
 }
---
+++ app/src/main/java/com/example/vanshika/concetto/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.vanshika.concetto;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_splash);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
+
+    }
+}
