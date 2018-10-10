45ada7919dcf41f3f9fb23b1a51f12e2ae99d7a3
Shridhar Goel <shridhar.goel@gmail.com>
2018-10-10 20:19:27 +0530

    Added navigation drawer in ContactUsActivity

--- app/src/main/java/com/example/vanshika/concetto/ContactUsActivity.java
+++ app/src/main/java/com/example/vanshika/concetto/ContactUsActivity.java
@@ -3,11 +3,13 @@ package com.example.vanshika.concetto;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ContactUsActivity extends AppCompatActivity {
+public class ContactUsActivity extends MainActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contact_us);
+        super.replaceContentLayout(R.layout.activity_contact_us, R.id.content_main_linear_layout);
+
+        getSupportActionBar().hide();
     }
 }
